d971cb805692ada82261c9dbd9aadb39eadabafd
Emma R <emrichardsone@gmail.com>
2021-10-04 15:23:30 -0500

    part 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-export function fetchMovies(){
+const movies = require('./data/movies.json'); 
+const fetchWithTimeout = require('./services');
 
+export function fetchMovies(){
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
 ;}
+
+const moviePromise = fetchMovies();
+moviePromise().then( function(result){
+    return console.log(result);
+});
+  
